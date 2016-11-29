1a81dd8c55a7f204e0268fa12c7956d207c8d935
smithla1 <smithla1@g.cofc.edu>
2016-11-28 20:38:33 -0500

    Added documentation

--- TestAutomation/project/sugar-calculate/functions.py
+++ TestAutomation/project/sugar-calculate/functions.py
@@ -410,7 +410,7 @@ sinc.__doc__ = _(
 'sinc(x), return the sinc of x. This is given by sin(x) / x.')
 
 def sqrt(x):
-    return math.sqrt(int(x))
+    return math.sqrt(int(x)) # return math.sqrt(float(x))
 sqrt.__doc__ = _(
 'sqrt(x), return the square root of x. This is the value for which the square \
 equals x. Defined for x >= 0.')
